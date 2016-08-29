dc1869c94333e871fbd52b82345c4426c90d672a
Daniel Haberbeck <daniel.haberbeck@gmail.com>
2016-08-29 14:31:09 -0300

    Update config.js
    correção de aspas

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ var config = {};
 config.twitter = {};
 config.cassandra = {};
 
-config.twitter.CONSUMER_KEY": "YOUR_CONSUMER_KEY",
-config.twitter.CONSUMER_SECRET": "YOUR_CONSUMER_SECRET",
-config.twitter.ACCESS_TOKEN_KEY": "YOUR_ACCESS_TOKEN_KEY",
-config.twitter.ACCESS_TOKEN_SECRET": "YOUR_ACCESS_TOKEN_SECRET",
+config.twitter.CONSUMER_KEY: "YOUR_CONSUMER_KEY",
+config.twitter.CONSUMER_SECRET: "YOUR_CONSUMER_SECRET",
+config.twitter.ACCESS_TOKEN_KEY: "YOUR_ACCESS_TOKEN_KEY",
+config.twitter.ACCESS_TOKEN_SECRET: "YOUR_ACCESS_TOKEN_SECRET",
 
 config.cassandra.CONTACTPOINTS = 'YOUR IP FROM CASSANDRA';
 config.cassandra.AUTHPROVIDER_USER = 'cassandra';
